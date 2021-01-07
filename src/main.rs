57eb81fd27fae91fd204bb17aff8a1596d644e1c
raimon <raimon49@hotmail.com>
2021-01-07 22:13:37 +0900

    Implement Add trait in self defined Complex<T> type

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,20 @@
 use std::ops::Add;
 
+pub struct Complex<T> {
+    pub re: T,
+    pub im: T,
+
+}
+
+impl<T> Add for Complex<T>
+    where T: Add<Output=T>
+{
+    type Output = Self;
+    fn add(self, rhs: Self) -> Self {
+        Complex { re: self.re, im: self.im + rhs.im }
+    }
+}
+
 fn main() {
     // std::ops::Addトレイトをスコープ内でuseすると
     // a + bをa.add(b)と関数呼び出しでも書ける
