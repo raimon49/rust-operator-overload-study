0ad16b105df0ef763aa850a0f35ac7c053140c7c
raimon <raimon49@hotmail.com>
2021-01-12 16:51:18 +0900

    Implement more generic function for Complex

--- src/main.rs
+++ src/main.rs
@@ -6,12 +6,23 @@ pub struct Complex<T> {
 
 }
 
-impl<T> Add for Complex<T>
-    where T: Add<Output=T>
+// impl<T> Add for Complex<T>
+//     where T: Add<Output=T>
+// {
+//     type Output = Self;
+//     fn add(self, rhs: Self) -> Self {
+//         Complex { re: self.re, im: self.im + rhs.im }
+//     }
+// }
+
+// 上のコメントアウトされたAdd for Complexを最大限ジェネリックにした実装
+// 実際にはLがAdd<R, Output=O>を実装する制約があるため、L, R, Oは同じ型になり、上の単純な実装と大差ない
+impl<L, R, O> Add<Complex<R>> for Complex<L>
+    where L: Add<R, Output=O>
 {
-    type Output = Self;
-    fn add(self, rhs: Self) -> Self {
-        Complex { re: self.re, im: self.im + rhs.im }
+    type Output = Complex<O>;
+    fn add(self, rhs: Complex<R>) -> Self::Output {
+        Complex { re: self.re + rhs.re, im: self.im + rhs.im }
     }
 }
 
